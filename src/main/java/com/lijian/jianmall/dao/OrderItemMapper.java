92cbb9141cff6420dbc9c61166ac4d671095e2e7
lijian <ljhhh@qq.com>
2021-08-21 02:18:19 +0800

    mybatis-generator使用命令：mvn mybatis-generator:generate

---
+++ src/main/java/com/lijian/jianmall/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package com.lijian.jianmall.dao;
+
+import com.lijian.jianmall.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
---
+++ src/main/java/com/lijian/jianmall/dao/OrderMapper.java
@@ -0,0 +1,17 @@
+package com.lijian.jianmall.dao;
+
+import com.lijian.jianmall.pojo.Order;
+
+public interface OrderMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    Order selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
