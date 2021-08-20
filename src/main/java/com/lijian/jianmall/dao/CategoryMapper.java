18863ec49d009f284d15a6e8c8ae759aa97ad560
lijian <ljhhh@qq.com>
2021-08-21 00:45:46 +0800

    mybatis，配置全局MapperScan，替代dao层的局部的@Mapper注解

--- src/main/java/com/lijian/jianmall/dao/CategoryMapper.java
+++ src/main/java/com/lijian/jianmall/dao/CategoryMapper.java
@@ -5,7 +5,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
-@Mapper
+//@Mapper
 public interface CategoryMapper {
     @Select("select * from mall_category where id = #{id}")
     Category findById(@Param("id") Integer id);
