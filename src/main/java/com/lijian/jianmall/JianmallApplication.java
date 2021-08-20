25661777f0ebd9f1283a3d1b1fffef176f26a204
lijian <ljhhh@qq.com>
2021-08-20 23:47:20 +0800

    初始化项目

---
+++ src/main/java/com/lijian/jianmall/JianmallApplication.java
@@ -0,0 +1,13 @@
+package com.lijian.jianmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JianmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JianmallApplication.class, args);
+    }
+
+}
